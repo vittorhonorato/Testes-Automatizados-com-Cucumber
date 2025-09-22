69f1c83f232596217ec10743918c1f8ff307744f
vittorhonorato <vittorhonoratoo@gmail.com>
2025-09-21 21:04:15 -0300

    wip: Iniciando Projeto

---
+++ src/test/java/com/vittorhonorato/integracao/Integracao/IntegracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vittorhonorato.integracao.Integracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
