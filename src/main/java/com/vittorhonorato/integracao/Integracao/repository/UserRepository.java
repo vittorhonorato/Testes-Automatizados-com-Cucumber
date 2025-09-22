7937799ed1c0334c11d48c5c94c0da6389108d1b
vittorhonorato <vittorhonoratoo@gmail.com>
2025-09-21 21:32:36 -0300

    wip: Criando repository

--- src/main/java/com/vittorhonorato/integracao/Integracao/repository/UserRepository.java
+++ src/main/java/com/vittorhonorato/integracao/Integracao/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.vittorhonorato.integracao.Integracao.repository;
 
 import com.vittorhonorato.integracao.Integracao.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
