4142b191589e14fbf773bf0d221a444ed8681077
vittorhonorato <vittorhonoratoo@gmail.com>
2025-09-21 21:54:01 -0300

    wip: Rotas GET e POST funcionando

--- src/main/java/com/vittorhonorato/integracao/Integracao/controller/UserController.java
+++ src/main/java/com/vittorhonorato/integracao/Integracao/controller/UserController.java
@@ -1,16 +1,28 @@
 package com.vittorhonorato.integracao.Integracao.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vittorhonorato.integracao.Integracao.entity.User;
+import com.vittorhonorato.integracao.Integracao.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public String test() {
-        return  "testando";
+    public ResponseEntity<List<User>> getAll() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveUser(user));
+    }
 }
--- src/main/java/com/vittorhonorato/integracao/Integracao/service/UserService.java
+++ src/main/java/com/vittorhonorato/integracao/Integracao/service/UserService.java
@@ -1,12 +1,23 @@
 package com.vittorhonorato.integracao.Integracao.service;
 
+import com.vittorhonorato.integracao.Integracao.entity.User;
 import com.vittorhonorato.integracao.Integracao.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }
