70a57a97a30ffeaf2c589d005a7dce4bfb56aa2c
vittorhonorato <vittorhonoratoo@gmail.com>
2025-09-21 21:16:45 -0300

    fix: entidade com atributo id alterada

--- src/main/java/com/vittorhonorato/integracao/Integracao/entity/User.java
+++ src/main/java/com/vittorhonorato/integracao/Integracao/entity/User.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class User {
 
-    private long id;
+    private Long id;
     private String name;
     private String email;
     private String password;
