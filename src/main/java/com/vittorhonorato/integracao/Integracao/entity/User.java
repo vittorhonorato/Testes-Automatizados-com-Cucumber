2fa0f301bd06100e9f6bfb063930b7f66666626b
vittorhonorato <vittorhonoratoo@gmail.com>
2025-09-21 21:33:01 -0300

    fix: Mapeando Entidade para h2

--- src/main/java/com/vittorhonorato/integracao/Integracao/entity/User.java
+++ src/main/java/com/vittorhonorato/integracao/Integracao/entity/User.java
@@ -1,5 +1,6 @@
 package com.vittorhonorato.integracao.Integracao.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Builder
+@Entity
+@Table(name = "users")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
